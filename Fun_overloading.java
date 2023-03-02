5495e3025660f4b0bb8475d4c14b66dcf33da5e2
PRKDevelops <124041839+PRKDevelops@users.noreply.github.com>
2023-03-02 22:37:20 +0530

    Add files via upload

---
+++ Fun_overloading.java
@@ -0,0 +1,15 @@
+
+public class Fun_overloading {
+	
+		public static int Sum(int a,int b){
+			return a+b;
+		}
+		public static float sum(float a,float b){
+			return a+b;
+		}
+		public static void main(String args[]){
+			System.out.println(Sum(2,3));
+			System.out.println(sum(3.7f,7.9f));
+		}
+	}
+
